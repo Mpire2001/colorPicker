1aa4e4ad52a2b8f2fdb867dfdf5bf6148f1979be
K S MITHUN <72300231+Mpire2001@users.noreply.github.com>
2021-08-17 19:11:44 +0530

    Add files via upload

---
+++ mithun.js
@@ -0,0 +1,12 @@
+function makePurple(){
+    document.body.style.backgroundColor="purple";
+}
+function makePgreen(){
+    document.body.style.backgroundColor="palegreen";
+}
+function makeYellow(){
+    document.body.style.backgroundColor="yellow";
+}
+function makePlum(){
+    document.body.style.backgroundColor="plum";
+}
